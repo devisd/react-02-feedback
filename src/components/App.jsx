46b052fa3f43a2a2fbad0b64b51f0c0c3ab72896
devisd <89788799+devisd@users.noreply.github.com>
2022-06-05 23:52:47 +0700

    add start files

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import { Feedback } from "./Feedback/Feedback";
+
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+        padding: 120,
+      }}
+    >
+
+    <Feedback />
+      
+    </div>
+  );
+};
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './Feedback.module.css'
+
+export class Feedback extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+}
+}
+
