1aab2f1ca4cb6a5c0e1b1b32a7eb565730aa2d79
devisd <89788799+devisd@users.noreply.github.com>
2022-06-13 23:32:33 +0700

    start

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,10 +3,52 @@ import './Feedback.module.css'
 
 export class Feedback extends Component {
 
+  static defaultProps = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
   state = {
     good: 0,
     neutral: 0,
     bad: 0
-}
+  }
+
+  updateStatisticGood = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  }
+
+  updateStatisticNeutral = () => {
+  this.setState(prevState => {
+    return { neutral: prevState.neutral + 1 };
+    });
+  }
+
+  updateStatisticBad = () => {
+  this.setState(prevState => {
+    return { bad: prevState.bad + 1 };
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+
+        <button type="button" onClick={this.updateStatisticGood}>Good</button>
+        <button type="button" onClick={this.updateStatisticNeutral}>Neutral</button>
+        <button type="button" onClick={this.updateStatisticBad}>Bad</button>
+
+        <h2>Statistics</h2>
+
+        <p>Good : {this.state.good}</p>
+        <p>Neutral : {this.state.neutral}</p>
+        <p>Bad : {this.state.bad}</p>
+      </div>
+    )
+  }
 }
 
